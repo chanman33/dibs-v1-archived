e6226308fcc2d37567aed57bf6dc65a83d600c4b
Chandler <chandler.j.ward@gmail.com>
2023-11-28 01:43:36 -0700

    Edit Home-4 Meta Data

--- app/(homes)/home-4/page.jsx
+++ app/(homes)/home-4/page.jsx
@@ -2,9 +2,9 @@ import dynamic from "next/dynamic";
 import HomeMain from "@/components/home-4";
 
 export const metadata = {
-  title: 'Home-4 || FindHouse - Real Estate React Template',
+  title: 'HomeHarbor - Real Estate & Agent Marketplace',
   description:
-    'FindHouse - Real Estate React Template',
+    'HomeHarbor - Real Estate & Agent Marketplace',
 }
 
 const index = () => {
